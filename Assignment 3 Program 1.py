7dfb132fac0e9356bfe68b9846ae44ca8503f576
OwenNSCC <worthenowen@gmail.comm>
2024-10-28 05:49:08 -0300

    finding max and most hours worked

--- Assignment 3 Program 1.py
+++ Assignment 3 Program 1.py
@@ -1,3 +1,8 @@
 # Adding the Number of Hours Worked
 dailyHours = [float(input(f"Enter hours worked on Day {i + 1}: ")) for i in range(5)]
 
+# Finding the Most Hours Worked
+mostHours = max(dailyHours)
+
+# Finding the Total Hours Worked
+totalHours = sum(dailyHours)

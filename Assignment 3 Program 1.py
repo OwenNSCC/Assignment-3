7ab1bfe60fe1004a3e9fc840ee72ce6bac15b58f
OwenNSCC <worthenowen@gmail.comm>
2024-10-28 03:15:00 -0300

    Adding Numbers of hours worked input

---
+++ Assignment 3 Program 1.py
@@ -0,0 +1,6 @@
+# Adding the Number of Hours Worked
+int(input("Enter hours worked on Day 1: "))
+int(input("Enter hours worked on Day 2: "))
+int(input("Enter hours worked on Day 3: "))
+int(input("Enter hours worked on Day 4: "))
+int(input("Enter hours worked on Day 5: "))

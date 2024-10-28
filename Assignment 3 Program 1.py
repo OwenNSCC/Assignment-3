0b064747255b8275d9535a5202608daff48024f0
OwenNSCC <worthenowen@gmail.comm>
2024-10-28 06:29:04 -0300

    hours worked average, Day with most hours, days slacked off

--- Assignment 3 Program 1.py
+++ Assignment 3 Program 1.py
@@ -6,3 +6,12 @@ mostHours = max(dailyHours)
 
 # Finding the Total Hours Worked
 totalHours = sum(dailyHours)
+
+# Finding Average Hours Worked
+averageHours = totalHours / 5
+
+# Finding Day with Most Hours
+mostHourDays = [i + 1 for i in range(5) if dailyHours[i] == mostHours]
+
+# Days Slacked off 
+slackedDays = [i + 1 for i in range(5) if dailyHours[i] < 7]

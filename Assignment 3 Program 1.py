00809b5a1c0f56d5fe129f8042170d1c334decb0
OwenNSCC <worthenowen@gmail.comm>
2024-10-28 05:39:31 -0300

    changed input daily hours

--- Assignment 3 Program 1.py
+++ Assignment 3 Program 1.py
@@ -1,6 +1,3 @@
 # Adding the Number of Hours Worked
-int(input("Enter hours worked on Day 1: "))
-int(input("Enter hours worked on Day 2: "))
-int(input("Enter hours worked on Day 3: "))
-int(input("Enter hours worked on Day 4: "))
-int(input("Enter hours worked on Day 5: "))
+dailyHours = [float(input(f"Enter hours worked on Day {i + 1}: ")) for i in range(5)]
+
